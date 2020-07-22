433942e15563331ab6fc176ab733441954269e4f
Antohan <justanton19@gmail.com>
2020-07-22 14:36:30 +0700

    Create simple brute force password feature.

--- Password Hacker/task/hacking/hack.py
+++ Password Hacker/task/hacking/hack.py
@@ -1,15 +1,31 @@
 import sys
 import socket
+import itertools
+
+address = (sys.argv[1], int(sys.argv[2]))
 
 
-ip_address = sys.argv[1]
-port = int(sys.argv[2])
-password = sys.argv[3]
+def generate_password():
+    index = 1
+    while True:
+        symbols = 'abcdefghijklmnopqrstuvwxyz0123456789'
+        yield from itertools.product(symbols, repeat=index)
+        index += 1
+
 
 with socket.socket() as sock:
-    sock.connect((ip_address, port))
-    send_data = password.encode()
-    sock.send(send_data)
-    response = sock.recv(1024)
-    data = response.decode()
-    print(data)
+    sock.connect(address)
+
+    for p in generate_password():
+        password = "".join(p)
+        sock.send(password.encode())
+
+        response = sock.recv(1024).decode()
+        if response == 'Connection success!':
+            print(password)
+            break
+        if response == 'Wrong password!':
+            continue
+        if response == 'Too many attempts':
+            print(response)
+            break
